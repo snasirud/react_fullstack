a326b5d982cfb5c96ad6565a4029af6110685bf5
snasirud <syed.nas103@hotmail.com>
2019-08-25 22:34:26 +1000

    Update secret.js
    sceret update

--- backend/secret.js
+++ backend/secret.js
@@ -1,5 +1,5 @@
 const secrets = {
-  dbUri: "mongodb://dbuser:dbuser123@cluster0-sh6tx.mongodb.net/test?retryWrites=true&w=majority"
+  dbUri: "mongodb+srv://dbuser:dbuser123@cluster0-sh6tx.mongodb.net/test?retryWrites=true&w=majority"
 };
 
 const getSecret = key => secrets[key];
